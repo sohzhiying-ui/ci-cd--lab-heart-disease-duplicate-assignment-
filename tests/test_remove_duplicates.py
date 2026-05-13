6535e70b4fc4ec56925b81d56cba23ce1c051bee
sohzhiying <sohzhiying@graduate.utm.my>
2026-05-13 12:22:08 +0800

    Create test_remove_duplicates.py

---
+++ tests/test_remove_duplicates.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pandas as pd
+from scripts.remove_duplicates import remove_duplicates
+
+def test_duplicates_removed():
+
+    input_file = "data/dataset.csv"
+    output_file = "data/test_output.csv"
+
+    remove_duplicates(input_file, output_file)
+
+    df = pd.read_csv(output_file)
+
+    assert df.duplicated().sum() == 0
